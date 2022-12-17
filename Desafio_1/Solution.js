9abf7ad6936c51ebe8dc1391698001a1e79651e7
yilmar Munioz <yilmar.munioz@hotmail.com>
2022-12-17 13:08:25 -0300

    Incorporación de nuevas validaciones

--- Desafio_1/Solution.js
+++ Desafio_1/Solution.js
@@ -1,12 +1,21 @@
 //Variable Edad
-let age = prompt ('¿Cuántos años tienes?');
-//
+let age = prompt ('¿Cuántos años tienes?',100);
+//Validar que sea un Numero
+//let ageFormat = parseInt(age, 10);
 if(age <= 16){
-  alert("No puede obtener la licencia de conducir")
+  alert("No puede obtener la licencia de conducir");
 }else{
   if(age == 17){
-    alert("Puede obtener la licencia de conducir con autorización de los padres o tutores y realizando las pruebas teoricas y prácticas")
+    alert("Puede obtener la licencia de conducir con autorización de los padres o tutores y realizando las pruebas teoricas y prácticas");
   }else{
-    alert("Puede obtener la licencia realizando las pruebas teoricas y prácticas")
+    if (age >= 18 && age <= 65){//Edad máx para sacar licencia en Chile
+       alert("Puede obtener la licencia realizando las pruebas teoricas y prácticas");
+      }else{
+        if(age>66){
+          alert("Su edad es mayor a la edad limite");
+        }else{
+          alert("Debe Ingressar una edad válida");
+        }
+      }  
   }
 }
